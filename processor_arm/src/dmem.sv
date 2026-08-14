d4802a8e8c52e394363338d1641ceb41631c3f5f
gvodanovic <gonzalovodanovic@gmail.com>
2026-08-14 18:03:23 -0300

    Initial Commit

---
+++ processor_arm/src/dmem.sv
@@ -0,0 +1,33 @@
+module dmem (input  logic        clk, memWrite, memRead,
+             input  logic [5:0]  address,
+             input  logic [63:0] writeData,
+             output logic [63:0] readData,
+             input  logic        dump);
+
+	localparam MAX_BOUND = 64;
+	localparam MEMORY_DUMP_FILE = "mem.dump";
+
+	logic [63:0] mem [MAX_BOUND-1:0];
+
+	always_ff @(posedge clk)
+		if (memWrite)
+			mem[address] <= writeData;
+
+	always_comb
+		if (memRead) readData = mem[address];
+		else         readData = '0;
+
+	always @(posedge dump) begin
+		integer dumpfile;
+		integer i;
+		dumpfile = $fopen(MEMORY_DUMP_FILE, "w");
+		$fdisplay(dumpfile, "Memoria RAM de Arm:");
+		$fdisplay(dumpfile, "Address Data");
+		for (i = 0; i <= MAX_BOUND-1; i = i+1) begin
+			$fdisplay(dumpfile, "%0d %016h", i, mem[i]);
+		end
+		$fclose(dumpfile);
+		$display("fin del testdump");
+	end
+
+endmodule
